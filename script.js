8b4c7bf4b6b202f70d2e254f319487192dffcc9f
Danisoftware <90097496+GeniusDanisoft@users.noreply.github.com>
2022-12-23 12:44:14 +0100

    XMas timer - mobile version
    A sample mobile XMas timer

---
+++ script.js
@@ -0,0 +1,37 @@
+var christmas_date = new Date('Dec 25, 2022 00:00:00')
+
+function countdown(){
+    var today = new Date()
+    var gap = christmas_date - today
+
+    var seconds = 1000
+    var minutes = seconds * 60
+    var hours = minutes * 60
+    var day = hours * 24
+
+    var d = Math.floor(gap / (day))
+    var h = Math.floor((gap % day) / (hours))
+    var m = Math.floor((gap % hours) / (minutes))
+    var s = Math.floor((gap % minutes) / (seconds))
+
+
+    document.getElementById("days").innerHTML = d
+    document.getElementById("hours").innerHTML = h
+    document.getElementById("minutes").innerHTML = m
+    document.getElementById("seconds").innerHTML = s
+    
+    
+    const h1 = document.querySelector("#giorno");
+    let uno = "1"
+    
+
+    if(h1 == uno){
+        h1.innerText = "cavoletto di bruxelles";
+    }
+
+}
+
+setInterval(()=>{
+    countdown()
+}, 1000)
+
